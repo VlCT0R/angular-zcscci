5b29f02f969737c1be8835bd1e647aa0496da54b
VlCT0R <vl.ammundsen@gmail.com>
2019-12-04 14:58:23 +1300

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,9 +2,16 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
+
 export class HeroService {
 
   constructor() { }
+  getHeroes(): Hero[] {
+  return HEROES;
+}
 
 }
+
